21aef86ae456a21083c3f5eaed91d3a07ecb89b1
Daniel <danielstarling4@gmail.com>
2019-03-20 12:55:08 -0700

    api online

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const server = express();
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("<h2>It's Working!!!</h2>");
+});
+module.exports = server;
